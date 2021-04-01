218206edb0d6c48772cc5e6587b5daf0d05c1f24
Tobias Nyholm <nyholm.tobias@gmail..se>
2021-04-01 15:18:53 +0200

    Commit 8 - creating a seeder

---
+++ Bicycle_Empire/HelpClasses/Seeder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bicycle_Empire
+{
+    static class Seeder
+    {
+        public static void FillData()
+        {
+            CustomersController cController = new CustomersController();
+            BicyclesController bController = new BicyclesController();
+            InvoiceInfoController iController = new InvoiceInfoController();
+            RentalOrdersController oController = new RentalOrdersController();
+            RentalPricesController pController = new RentalPricesController();
+
+            string[] firstNames = { "Tobias", "Oliver", "Marcus", "Ninmar", "Robin", "Erik", "Johan", "Anders", "Johanna", "Sofia" };
+            string[] lastNames = { "Nyholm", "Stenström", "Medina", "Karlsson", "Kamo", "Niklasson", "Johansson", "Svensson", "Enberg", "Svensson" };
+            int[] phoneNumers = { 0768645167, 0763669581, 0758981235, 0752125886, 0735669852, 0752778412, 0769721001, 0705723698, 0768645112, 0758996521 };
+
+            for(int i = 0; i < 10; i++)
+            {
+                cController.Add(new Customers { first_name = firstNames[i], last_name = lastNames[i], phone_number = phoneNumers[i] });
+            }
+        }
+
+    }
+}
--- Bicycle_Empire/Program.cs
+++ Bicycle_Empire/Program.cs
@@ -3,9 +3,10 @@
     class Program
     {
         // Jag har skrivit de flesta kommentarerna i klasserna Menu och CustomersController.
-        // Strukturen är så att alla "Controller" klasser har hand om anropen till och från databasen. Menu klassen är bara en stödklass som har hand om utskrifter. 
+        // Strukturen är så att alla "Controller" klasser har hand om anropen till och från databasen. Menu och Seeder klasserna är bara stödklasser som har hand om utskrifter och fyller på databasen. 
         // Model är POCO klasser och är bara ramar för de objekt som hanteras av controller klasserna.  
-        // De "vanliga" valen i huvudmenyn är till för att visa på att jag kan göra en CRUD-databas, medan valet "Real life example" är en kort simulation av hur ett verkligt företag hade kunnat använda sig av mjukvaran och databasen.
+        // De "vanliga" valen i huvudmenyn är till för att visa på att jag kan göra en CRUD-databas.
+        // Valet "Real life example" är en kort simulation av hur ett verkligt företag hade kunnat använda sig av mjukvaran och databasen.
         static void Main(string[] args)
         {
             Menu.PrintMainMenu();
